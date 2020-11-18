38984a98fbe39670f626caf2d88142afdb4f64da
AmnaAhme7 <72925659+AmnaAhme7@users.noreply.github.com>
2020-11-18 14:06:12 +0200

    Initial Commit

---
+++ NewsApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NewsApp
+//
+//  Created by Amna on 11/18/20.
+//  Copyright © 2020 Amna. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
